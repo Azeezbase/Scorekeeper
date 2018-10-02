a220baf415bdd143950bf4848b8fba717f479702
Azeez Riliwan Omobolaji <omobolajiridwan1993@gmail.com>
2018-10-03 11:18:19 +1400

    Initial commit

---
+++ app/src/main/java/com/example/omobolaji/scorekeeper/ApplicationTestCase.java
@@ -0,0 +1,8 @@
+package com.example.omobolaji.scorekeeper;
+
+import android.app.Application;
+
+class ApplicationTestCase<T> {
+    public ApplicationTestCase(Class<Application> applicationClass) {
+    }
+}
